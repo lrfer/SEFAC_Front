28968bfce970ab9e2be218c8967999b62c010372
Luiz Fernando <lr_fernando@hotmail.com>
2023-05-15 16:44:47 -0300

    New cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // URL da API
+  // URL_API: 'http://200.131.206.12/scad/api',
+  // URL_API: 'http://localhost/scad/api',
+  URL_API: 'http://lrfer1-001-site1.htempurl.com/api',
+
+  // Endpoints
+  LOGIN: '/Usuario/autenticar',
+  ALUNO : '/Aluno',
+  EXECUCAO_ATIVIDADE: '/ExecucaoAtividade',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,12 @@ export const environment = {
   // URL da API
   // URL_API: 'http://200.131.206.12/scad/api',
   // URL_API: 'http://localhost/scad/api',
-   URL_API: 'https://localhost:7051/api',
+   URL_API: 'http://lrfer1-001-site1.htempurl.com/api',
 
   // Endpoints
   LOGIN: '/Usuario/autenticar',
   ALUNO : '/Aluno',
   EXECUCAO_ATIVIDADE: '/ExecucaoAtividade',
+
+
 };
